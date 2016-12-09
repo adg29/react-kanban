cf52caa22e9ea2cf0948a00ead64ed6bfcb84315
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-09 01:59:27 -0500

    first commit

---
+++ app/Checklist.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CheckList extends Component {
+  render() {
+    let tasks = this.props.tasks.map((task) => ( <li key={task.id} className="checklist__task">
+                                                   <input type="checkbox" defaultChecked={ task.done } />
+                                                   { task.name }
+                                                   <a href="#" className="checklist__task--remove" />
+                                                 </li> ));
+
+    return (
+      <div className="checklist">
+        <ul>
+          { tasks }
+        </ul>
+      </div>
+      );
+  }
+}
+export default CheckList;
