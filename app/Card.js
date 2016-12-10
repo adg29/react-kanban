9af07a66abc13ac773cdf9a4746ace351ce138bd
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-10 15:47:11 -0500

    add a className conditionally to the Card Title with ternary expression

--- app/Card.js
+++ app/Card.js
@@ -27,7 +27,7 @@ class Card extends Component {
       )
     }
     return ( <div className="card">
-               <div className="card__title" onClick={this.toggleDetails.bind(this)}>
+               <div className={ this.state.showDetails ? "card__title card__title--is-open" : "card__title" } onClick={ this.toggleDetails.bind(this) }>
                  { this.props.title }
                </div>
                { cardDetails }
