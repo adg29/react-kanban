1df4395fd2303698f205bd8779cafa52ee6a0ccc
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-11 14:00:16 -0500

    create a new component at the top the hierarchy. This container component will be used for data fetching/persistence. also install pollyfill for window.fetch

---
+++ app/KanbanBoardContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
+class KanbanBoardContainer extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      cards: [],
+    };
+  }
+
+  render() {
+    return <KanbanBoard cards={ cards } />
+  }
+}
+export default KanbanBoardContainer;
