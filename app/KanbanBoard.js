22b66a2c19b91d289cc80e0cd123d4d9dee579f8
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-10 23:07:14 -0500

    Import PropTypes and define them for KanbanBoard and List

--- app/KanbanBoard.js
+++ app/KanbanBoard.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import List from './List';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-class KanbanBoard extends Component{
-	render(){
-		return (
-			<div className="app">
-				<List id='todo' title="To Do" cards={ this.props.cards.filter((card) => card.status === "todo")} />
-				<List id='in-progress' title="In Progress" cards={ this.props.cards.filter((card) => card.status === "in-progress")} />
-				<List id='done' title="Done" cards={ this.props.cards.filter((card) => card.status === "done")} />
-			</div> );
-	}
+class KanbanBoard extends Component {
+  render() {
+    return (
+      <div className="app">
+        <List id='todo' title="To Do" cards={ this.props.cards.filter((card) => card.status === "todo") } />
+        <List id='in-progress' title="In Progress" cards={ this.props.cards.filter((card) => card.status === "in-progress") } />
+        <List id='done' title="Done" cards={ this.props.cards.filter((card) => card.status === "done") } />
+      </div> );
+  }
 }
+
+KanbanBoard.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object)
+};
+
 
 export default KanbanBoard;
--- app/List.js
+++ app/List.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Card from './Card';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-class List extends Component { 
-	render() {
-		var cards = this.props.cards.map((card) => { 
-			return <Card key={card.id} id={card.id}
-                   title={card.title}
-                   color={card.color}
-                   description={card.description}
-                   tasks={card.tasks} />
-		});
+class List extends Component {
+  render() {
+    var cards = this.props.cards.map((card) => {
+      return <Card key={ card.id } id={ card.id } title={ card.title } color={ card.color } description={ card.description } tasks={ card.tasks }
+             />
+    });
 
-		return (
-			<div className="list">
-				<h1>{this.props.title}</h1>
-				{cards} 
-			</div>
-		); 
-	}
+    return (
+      <div className="list">
+        <h1>{ this.props.title }</h1>
+        { cards }
+      </div>
+      );
+  }
 }
+
+List.propTypes = {
+  title: PropTypes.string.isRequired,
+  cards: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default List;
