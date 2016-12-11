e1b24a3dd25a1f52ccd8d6607129143063e3d665
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-11 00:38:01 -0500

    custom PropType validator to warn if the card title is longer than 80 characters.

--- app/Card.js
+++ app/Card.js
@@ -2,6 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import CheckList from './CheckList';
 import marked from 'marked';
 
+let titlePropType = (props, propName, componentName) => {
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value !== 'string' || value.length > 80) {
+      return new Error(
+        `${propName} in ${componentName}  is longer than 80 characters`
+      );
+    }
+  }
+}
+
 class Card extends Component {
   constructor() {
     super(...arguments);
@@ -50,7 +61,7 @@ class Card extends Component {
 
 Card.propTypes = {
   id: PropTypes.number,
-  title: PropTypes.string,
+  title: titlePropType,
   description: PropTypes.string,
   color: PropTypes.string,
   tasks: PropTypes.arrayOf(PropTypes.object)
