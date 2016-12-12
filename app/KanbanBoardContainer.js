d2f79b6a9fc4eb0084fb04eac2d77fe35a294f4f
Alan Garcia <alandavidgarcia@gmail.com>
2016-12-12 01:34:45 -0500

    import update from immutability helper to be able to update the cards object

--- app/KanbanBoardContainer.js
+++ app/KanbanBoardContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
 import KanbanBoard from './KanbanBoard';
 
 const API_URL = 'http://kanbanapi.pro-react.com';
@@ -17,20 +18,26 @@ class KanbanBoardContainer extends Component {
 
   addTask(cardId, taskName) {}
   deleteTask(cardId, taskId, taskIndex) {
-  	// Find the index of the card
-let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
-// Create a new object without the task 
-let nextState = update(this.state.cards, {
-                            [cardIndex]: {
-                              tasks: {$splice: [[taskIndex,1]] }
-} });
-// set the component state to the mutated object this.setState({cards:nextState});
-// Call the API to remove the task on the server
-fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
-  method: 'delete',
-  headers: API_HEADERS
-});
-  
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card) => card.id == cardId);
+    // Create a new object without the task 
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: {
+          $splice: [[taskIndex, 1]]
+        }
+      }
+    });
+    // set the component state to the mutated object 
+    this.setState({
+      cards: nextState
+    });
+    // Call the API to remove the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'delete',
+      headers: API_HEADERS
+    });
+
   }
   toggleTask(cardId, taskId, taskIndex) {}
 
